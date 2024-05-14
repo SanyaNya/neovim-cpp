c728f7782fcefc56320a957490f884895772fd56
SanyaNya <sanyanya4ever@gmail.com>
2024-05-15 02:32:00 +0300

    Simplify installation so user dont need to wait for end multiple times

--- install.sh
+++ install.sh
@@ -30,6 +30,8 @@ if [ "$ID" = "arch" ]; then
     esac
 fi
 
+echo -e "${WHITE}The Script will open neovim to install plugins. ${RED} after installation was done, quit using :qa command${NC}"
+
 #  Backup Operation
 echo -e "${GREEN}-==Backup Operation==-${NC}"
 # Ask user if they want to create a backup
@@ -57,39 +59,5 @@ git clone https://github.com/NvChad/starter ~/.config/nvim --depth 1
 echo -e "${GREEN}-==Copying Files==-${NC}"
 rsync -av --exclude='README.md' --exclude='.git/' --exclude='install.sh' . ~/.config/nvim/lua/
 
-# open NvChad for the first time to download lazy plugin
-echo -e "${WHITE}The Script will open neovim to install lazy plugin. ${RED} after installation was done, quit using :qa command${NC}"
-
-# Ask user for confirmation
-read -p "type (yes) to continue => " response
-case "$response" in
- [yY][eE][sS]|[yY]) 
-     # open Neovim
-     nvim
-     ;;
-esac
-
-#  Adding Command
-# Display a warning message (for MasonInstallAll)
-echo -e "${WHITE}Neovim is about to open to install needed packages, after it downloaded the packages, quit neovim with :qa command! and wait for script to do the rest.${NC}"
-
-# Ask user for confirmation
-read -p "Do you understand the instructions? (yes/no) " response
-case "$response" in
- [yY][eE][sS]|[yY]) 
-     echo -e "${GREEN}-==Adding MasonInstallAll Command==-${NC}"
-     # Append the MasonInstallAll command to the init.lua file
-     echo 'vim.cmd("MasonInstallAll")' >> ~/.config/nvim/init.lua
-     # Open Neovim
-     nvim
-     ;;
- *)
-     echo -e "${RED}Exiting...${NC}"
-     exit 1
-     ;;
-esac
-
-#  Removing Command
-echo -e "${GREEN}-==Removing MasonInstallAll Command==-${NC}"
-# Remove the command from init.lua
-sed -i '/^vim.cmd("MasonInstallAll")$/d' ~/.config/nvim/init.lua
+nvim "+Lazy! sync" "+qa"
+nvim "+MasonInstallAll"
