33e6128cbab4023b8e6ea0653b831ff1546b72e5
SanyaNya <sanyanya4ever@gmail.com>
2024-05-18 22:02:20 +0300

    Display dap mappings in DAP group in cheatsheet

--- mappings.lua
+++ mappings.lua
@@ -9,8 +9,8 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- Migrating your dap mappings
-map("n", "<leader>db", "<cmd> DapToggleBreakpoint <CR>", { desc = "Add breakpoint at line" })
-map("n", "<leader>dr", "<cmd> DapContinue <CR>", { desc = "Start or continue the debugger" })
+map("n", "<leader>db", "<cmd> DapToggleBreakpoint <CR>", { desc = "DAP Add breakpoint at line" })
+map("n", "<leader>dr", "<cmd> DapContinue <CR>", { desc = "DAP Start or continue the debugger" })
 
 -- Uncomment and adjust the save mapping as needed
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
